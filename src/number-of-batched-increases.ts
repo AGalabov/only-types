7bc6ed63132785e26012b2645aa9650edf791c23
Alexandar Galabov <galabovalexandar@gmail.com>
2022-08-13 18:55:41 +0300

    formatting

--- src/number-of-batched-increases.ts
+++ src/number-of-batched-increases.ts
@@ -1,5 +1,5 @@
-import { RestFromArray } from "../lib/arrays";
-import { Length, Zero, GreaterThan, PlusOne } from "../lib/numbers";
+import { RestFromArray } from '../lib/arrays';
+import { Length, Zero, GreaterThan, PlusOne } from '../lib/numbers';
 
 export type NumberOfBatchedIncreases<
   Input extends number[],
@@ -15,8 +15,24 @@ export type NumberOfBatchedIncreases<
   : Accumulator;
 
 type SimpleSample = [1, 2, 4, 3, 6, 5, 7, 8, 5, 6, 8];
-type LargerSample = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263, 265, 300, 19, 18, 20, 21];
-
+type LargerSample = [
+  199,
+  200,
+  208,
+  210,
+  200,
+  207,
+  240,
+  269,
+  260,
+  263,
+  265,
+  300,
+  19,
+  18,
+  20,
+  21
+];
 
 type Solution1 = NumberOfBatchedIncreases<SimpleSample>; // Solution is of type  6
 type Solution2 = NumberOfBatchedIncreases<LargerSample>; // Solution2 is of type 8
