3bb125b196588fac115262a02d131ea467e4bc50
Alexander Galabov <galabovalexandar@gmail.com>
2024-03-06 13:58:34 +0200

    added unwrap for model from a different base

--- lib/unwrap-objection-model.ts
+++ lib/unwrap-objection-model.ts
@@ -1,6 +1,18 @@
-import type { Model } from 'objection';
+import { Model } from 'objection';
 
-export type UnwrapModelData<T extends typeof Model> = Omit<
-  T['prototype'],
-  keyof typeof Model['prototype']
->;
+class BaseModel extends Model {
+  id!: number;
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export type UnwrapModelDataFromBase<
+  M extends typeof Model,
+  Base extends typeof Model = typeof Model
+> = Omit<M['prototype'], keyof Base['prototype']>;
+
+export type UnwrapModelData<T extends typeof Model> =
+  UnwrapModelDataFromBase<T>;
+
+export type UnwrapModelInsertData<T extends typeof BaseModel> =
+  UnwrapModelDataFromBase<T, typeof BaseModel>;
