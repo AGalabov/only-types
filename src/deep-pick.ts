2e01fc910b441619b802a8fa02698a5fc9a0971f
Alexander Galabov <galabovalexandar@gmail.com>
2025-03-13 10:43:11 +0200

    fix union

--- src/deep-pick.ts
+++ src/deep-pick.ts
@@ -12,28 +12,36 @@ type AllowedInputs<T> = T extends object ? {
 }[keyof T] : never;
 
 type DeepPickWithoutSuggestions<T, K extends string> =
-  T extends object ? {
-  [P in keyof T as P extends Head<K> ? P : never]:
-    P extends Head<K>
-      ? T[P] extends readonly unknown[]
-        ? DeepPickWithoutSuggestions<T[P][number], Tail<Extract<K, `${P}.${string}`>>>[]
-        : DeepPickWithoutSuggestions<T[P], Tail<Extract<K, `${P}.${string}`>>>
-      : never
-} : T
+  T extends object ? Head<K> extends never
+    ? { [P in keyof T]: T[P] }
+    : {
+      [P in keyof T as P extends Head<K> ? P : never]:
+        P extends Head<K>
+          ? T[P] extends readonly unknown[]
+            ? DeepPickWithoutSuggestions<T[P][number], Tail<Extract<K, `${P}.${string}`>>>[]
+            : DeepPickWithoutSuggestions<T[P], Tail<Extract<K, `${P}.${string}`>>>
+          : T[P]
+    }
+  : T
 
-type DeepPick<T, K extends AllowedInputs<T>> = K extends string ? DeepPickWithoutSuggestions<T, K> : never;
 
-type TestData = {
-  asd: string,
-  b?: number,
-  q: {
-    test: string,
-    c?: number
+type UnionToIntersection<U> = (U extends any ? (arg: U) => any : never) extends ((arg: infer I) => any) ? I : never
+
+type DeepPick<T, K extends AllowedInputs<T>> = UnionToIntersection<K extends infer Keys ? Keys extends string ? DeepPickWithoutSuggestions<T, Keys> : never : never>;
+
+type User = {
+  name: string,
+  age?: number,
+  address: {
+    street: string,
+    zipCode?: number
   }
-  qwe: {
-    asd: string,
-    b?: number,
+  posts: {
+    title: string,
+    likes?: number,
   }[],
 };
 
-type Test = DeepPick<TestData, 'asd' | 'q' | 'qwe.asd' | 'b'>;
+// Important: This is a single object (no unions) and it supports specific
+// paths ONLY - just like the regular Pick
+type UserSubset = DeepPick<User, 'address' | 'name' | 'posts.likes'>;
